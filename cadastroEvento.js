7c4eb98da38ab5ba04d8d11ce15429f8b1dd28ac
felipe.ferreira <fehenr@live.com>
2021-09-02 20:34:18 -0300

    primeiro arquivo

---
+++ cadastroEvento.js
@@ -0,0 +1,31 @@
+console.log ("Você irá cadastrar seu evento aqui.")
+console.log ("Qual seu nome?")
+
+let nome = ["Felipe"];
+
+console.log ("Digite a data do evento:")
+
+var dataEvento = ("05/09/2021")
+if (dataEvento > "02/09/2021"){
+    console.log ("Data disponível.")
+} else {
+    console.log ("Data anterior a data atual.")
+}
+
+console.log ("Digite sua idade")
+var idade = 27
+
+if (idade >= 18){
+    console.log ("Estamos terminando.")
+} else {
+    console.log ("Não permitido para menores de 18 anos.")
+}
+
+console.log ("Qual o número de participantes?")
+var participantes = 99
+
+if (participantes <= 100){
+    console.log ("Cadastro realizado com sucesso!")
+} else {
+    console.log ("O número de participantes não é permitido.")
+}
